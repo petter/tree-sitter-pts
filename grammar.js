1c3a5951990e27da4999b81ccdf34ca2a7399088
Petter Sæther Moen <pettersmoen@gmail.com>
2020-11-05 14:12:15 +0100

    Proper setup

--- grammar.js
+++ grammar.js
@@ -1,11 +1,13 @@
-module.exports = grammar(require('tree-sitter-typescript/typescript'), {
-    name: 'pts',
-    rules: {
-        _declaration: ($, previous) => choice(...[...prevous.members, $.template_declaration],
-        template_declaration: $ => seq(
-            'template',
-            field('name', $.identifier),
-            field('body', $.statement_block)
-        )
-    }
+module.exports = grammar(require("tree-sitter-typescript/typescript/grammar"), {
+  name: "pts",
+  externals: false,
+  rules: {
+    _declaration: ($, previous) => choice(previous, $.template_declaration),
+    template_declaration: ($) =>
+      seq(
+        "template",
+        field("name", $.identifier),
+        field("body", $.statement_block)
+      ),
+  },
 });
