5dfd26a4c783a85249f41b7d14269a88ee1862c2
Petter Sæther Moen <pettersmoen@gmail.com>
2020-11-09 13:35:45 +0100

    Fjerne unødvendig felt

--- grammar.js
+++ grammar.js
@@ -1,6 +1,5 @@
 module.exports = grammar(require('tree-sitter-typescript/typescript/grammar'), {
   name: 'pts',
-  externals: false,
   rules: {
     _declaration: ($, previous) => choice(previous, $.template_declaration),
 
