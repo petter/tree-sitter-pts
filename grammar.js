1b7ef533f6ac39da3678f1227305aa1c90472e3a
Petter Sæther Moen <pettersmoen@gmail.com>
2020-11-09 13:33:19 +0100

    Core-PT gramatikk ferdig

--- grammar.js
+++ grammar.js
@@ -3,11 +3,41 @@ module.exports = grammar(require("tree-sitter-typescript/typescript/grammar"), {
   externals: false,
   rules: {
     _declaration: ($, previous) => choice(previous, $.template_declaration),
+
     template_declaration: ($) =>
       seq(
         "template",
         field("name", $.identifier),
-        field("body", $.statement_block)
+        field("body", $.package_template_body)
       ),
+    package_declaration: ($) =>
+      seq(
+        "package",
+        field("name", $.identifier),
+        field("body", $.package_template_body)
+      ),
+
+    package_template_body: ($) => seq("{", repeat($.inst_statement), "}"),
+    package_template_body_decl: ($) =>
+      choice(
+        $.inst_statement,
+        $.addto_statement,
+        $.class_declaration,
+        $.interface_declaration
+      ),
+
+    inst_statement: ($) =>
+      seq(
+        "inst",
+        field("template_name", $.identifier),
+        optional(seq("{", repeat1($.class_rename), "}", ";"))
+      ),
+    class_rename: ($) =>
+      seq($.rename, optional(seq("(", repeat1($.rename), ")"))),
+    rename: ($) =>
+      seq(field("old", $.identifier), "->", field("new", $.identifier)),
+
+    addto_statement: ($) =>
+      seq("addto", field("name", $.identifier), $.class_body, ";"),
   },
 });
